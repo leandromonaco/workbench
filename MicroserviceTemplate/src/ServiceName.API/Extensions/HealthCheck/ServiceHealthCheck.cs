5ba18f881f8ff66032d37be2eef4c2260bc7d1ec
leandromonaco <leandro.monaco@gmail.com>
2022-06-15 14:37:59 +1000

    [MicroserviceTemplate] Added HealthCheck Support

---
+++ MicroserviceTemplate/src/ServiceName.API/Extensions/HealthCheck/ServiceHealthCheck.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace ServiceName.API.Extensions.HealthCheck
+{
+    public class CustomServiceHealthCheck : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            var isHealthy = true;
+
+            if (isHealthy)
+            {
+                return Task.FromResult(
+                    HealthCheckResult.Healthy("A healthy result."));
+            }
+
+            return Task.FromResult(
+                new HealthCheckResult(
+                    context.Registration.FailureStatus, "An unhealthy result."));
+        }
+    }
+}
---
+++ MicroserviceTemplate/src/ServiceName.API/Extensions/HealthCheckExtensions.cs
@@ -0,0 +1,50 @@
+﻿using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using ServiceName.API.Extensions.HealthCheck;
+
+namespace ServiceName.API.Extensions
+{
+    /// <summary>
+    /// https://docs.steeltoe.io/api/v3/management/health.html
+    /// https://docs.microsoft.com/en-us/aspnet/core/host-and-deploy/health-checks?view=aspnetcore-6.0
+    /// https://docs.microsoft.com/en-us/dotnet/architecture/microservices/implement-resilient-applications/monitor-app-health
+    /// https://blog.zhaytam.com/2020/04/30/health-checks-aspnetcore/
+
+    //AspNetCore.HealthChecks.UI which adds the UI.
+    //AspNetCore.HealthChecks.UI.Client which turns our old response(e.g.Healthy) into a more detailed response.
+    //AspNetCore.HealthChecks.UI.InMemory.Storage which saves the results in memory for the UI to use.
+
+    /// </summary>
+    public static class HealthCheckExtensions
+    {
+        public static void AddHealthCheckSupport(this IServiceCollection services)
+        {
+            services.AddHealthChecks()
+                    .AddCheck("AlwaysHealthy", () => HealthCheckResult.Healthy())
+                    .AddCheck<CustomServiceHealthCheck>("ServiceHealthCheck")
+                    .AddSqlServer("")
+                    .AddRedis("");
+                    //.AddDynamoDb("");
+
+            services.AddHealthChecksUI(s =>
+            {
+                s.AddHealthCheckEndpoint("ServiceName", "/health");
+            }).AddInMemoryStorage();
+        }
+
+        public static void ConfigureHealthCheck(this WebApplication app)
+        {
+            app.UseHealthChecksUI(config => config.UIPath = "/health-ui");
+
+            app.MapHealthChecksUI();
+
+            app.MapHealthChecks("/health", new HealthCheckOptions()
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
+                AllowCachingResponses = false
+            });
+        }
+    }
+}
