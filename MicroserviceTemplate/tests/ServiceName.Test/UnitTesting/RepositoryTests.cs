10ce28ac884c65f916093638f82a7eab51eb4853
leandromonaco <leandro.monaco@gmail.com>
2022-06-24 15:15:59 +1000

    [MicroserviceTemplate] Moved tye.yaml Added unit test and renamed some others

--- MicroserviceTemplate/tests/ServiceName.Test/UnitTesting/RepositoryTests.cs
+++ MicroserviceTemplate/tests/ServiceName.Test/UnitTesting/RepositoryTests.cs
@@ -21,7 +21,7 @@ namespace ServiceName.Test.UnitTesting
         }
 
         [Fact]
-        public async Task SettingRepositoryTestWhenTenantIdExists()
+        public async Task SettingRepositoryTestWhenTenantIdExistsAllTrue()
         {
             var tenantId = "53a13ec4-fde8-4087-8e2a-5fb6b1fbc062";
 
@@ -32,6 +32,20 @@ namespace ServiceName.Test.UnitTesting
             Assert.NotNull(result.CategoryA);
             Assert.True(result.CategoryA.IsSettingAEnabled);
             Assert.True(result.CategoryA.IsSettingBEnabled);
+        }
+
+        [Fact]
+        public async Task SettingRepositoryTestWhenTenantIdExistsTrueFalse()
+        {
+            var tenantId = "570e85dd-17a1-49c1-851c-623345a731a3";
+
+            _autoMocker.GetMock<IDynamoDBContext>().Setup(x => x.LoadAsync<SettingDbRecord>(tenantId, default)).ReturnsAsync(_testHelper.GetDynamoDBRecord(tenantId));
+
+            var result = await _mockSettingsRepository.GetByIdAsync(Guid.Parse(tenantId));
+
+            Assert.NotNull(result.CategoryA);
+            Assert.True(result.CategoryA.IsSettingAEnabled);
+            Assert.False(result.CategoryA.IsSettingBEnabled);
         }
 
         [Fact]
@@ -49,9 +63,23 @@ namespace ServiceName.Test.UnitTesting
         }
 
         [Fact]
-        public async Task SettingRepositoryTestWhenSettingsAreEmpty()
+        public async Task SettingRepositoryTestWhenTenantIdExistsAndSettingsAreEmpty()
         {
             var tenantId = "e2c92679-5309-438b-8efc-64054a7babc2";
+
+            _autoMocker.GetMock<IDynamoDBContext>().Setup(x => x.LoadAsync<SettingDbRecord>(tenantId, default)).ReturnsAsync(_testHelper.GetDynamoDBRecord(tenantId));
+
+            var result = await _mockSettingsRepository.GetByIdAsync(Guid.Parse(tenantId));
+
+            Assert.NotNull(result.CategoryA);
+            Assert.False(result.CategoryA.IsSettingAEnabled);
+            Assert.False(result.CategoryA.IsSettingBEnabled);
+        }
+
+        [Fact]
+        public async Task SettingRepositoryTestWhenTenantIdExistsAndSettingsAreNull()
+        {
+            var tenantId = "71b83bab-a714-4fb0-a2bb-c06e5c7cfb9b";
 
             _autoMocker.GetMock<IDynamoDBContext>().Setup(x => x.LoadAsync<SettingDbRecord>(tenantId, default)).ReturnsAsync(_testHelper.GetDynamoDBRecord(tenantId));
 
