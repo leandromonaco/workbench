aa10ca5640d502bf184154cca9cef342b05bb2f2
leandromonaco <leandro.monaco@gmail.com>
2022-06-24 15:03:43 +1000

    [MicroserviceTemplate] Fixed KMS local url Improved Test project Moved tye.yaml to root folder

---
+++ MicroserviceTemplate/tests/ServiceName.Test/IntegrationTesting/JwtTests.cs
@@ -0,0 +1,56 @@
+﻿using Moq.AutoMock;
+using ServiceName.Core.Model;
+using ServiceName.Infrastructure.Authentication;
+using ServiceName.Test.Helpers;
+
+namespace ServiceName.Test.IntegrationTesting
+{
+    public class JwtTests
+    {
+        private AssymetricKmsJwtService _assymetricKmsJwtService;
+        private ModuleIdentity _moduleIdentity;
+        private TestHelper _testHelper;
+
+        public JwtTests()
+        {
+            var _autoMocker = new AutoMocker();
+
+            _moduleIdentity = new()
+            {
+                UserGuid = "UserGuid",
+                InstanceGuid = "InstanceGuid",
+                UserName = "UserName"
+            };
+
+            _testHelper = new TestHelper();
+
+            _autoMocker.Use(_testHelper.GetAmazonKms());
+            _autoMocker.Use(_testHelper.Configuration);
+            _assymetricKmsJwtService = _autoMocker.CreateInstance<AssymetricKmsJwtService>();
+        }
+
+        [Fact]
+        public async Task JwtValidationOK()
+        {
+            var token = await _assymetricKmsJwtService.GenerateTokenAsync(_moduleIdentity, 60, "issuer", "audience");
+            var isValid = await _assymetricKmsJwtService.ValidateTokenAsync(token);
+            Assert.True(isValid);
+        }
+
+        [Fact]
+        public async Task JwtWithIssuerMismatch()
+        {
+            var token = await _assymetricKmsJwtService.GenerateTokenAsync(_moduleIdentity, 60, "Issuer", "Audience");
+            var isValid = await _assymetricKmsJwtService.ValidateTokenAsync(token);
+            Assert.False(isValid);
+        }
+
+        [Fact]
+        public async Task JwtWithAudienceMismatch()
+        {
+            var token = await _assymetricKmsJwtService.GenerateTokenAsync(_moduleIdentity, 60, "Issuer2", "Audience2");
+            var isValid = await _assymetricKmsJwtService.ValidateTokenAsync(token);
+            Assert.False(isValid);
+        }
+    }
+}
--- MicroserviceTemplate/tests/ServiceName.Test/UnitTesting/RepositoryTests.cs
+++ MicroserviceTemplate/tests/ServiceName.Test/UnitTesting/RepositoryTests.cs
@@ -5,17 +5,19 @@ using ServiceName.Infrastructure.Repositories;
 using ServiceName.Infrastructure.Repositories.DynamoDBModel;
 using ServiceName.Test.Helpers;
 
-namespace ServiceName.Test
+namespace ServiceName.Test.UnitTesting
 {
     public class RepositoryTests
     {
         private readonly AutoMocker _autoMocker;
         private readonly SettingsRepositoryService _mockSettingsRepository;
+        private TestHelper _testHelper;
 
         public RepositoryTests()
         {
             _autoMocker = new AutoMocker();
             _mockSettingsRepository = _autoMocker.CreateInstance<SettingsRepositoryService>();
+            _testHelper = new TestHelper();
         }
 
         [Fact]
@@ -23,7 +25,7 @@ namespace ServiceName.Test
         {
             var tenantId = "53a13ec4-fde8-4087-8e2a-5fb6b1fbc062";
 
-            _autoMocker.GetMock<IDynamoDBContext>().Setup(x => x.LoadAsync<SettingDbRecord>(tenantId, default)).ReturnsAsync(TestHelper.GetDynamoDBRecord(tenantId));
+            _autoMocker.GetMock<IDynamoDBContext>().Setup(x => x.LoadAsync<SettingDbRecord>(tenantId, default)).ReturnsAsync(_testHelper.GetDynamoDBRecord(tenantId));
 
             var result = await _mockSettingsRepository.GetByIdAsync(Guid.Parse(tenantId));
 
@@ -37,7 +39,7 @@ namespace ServiceName.Test
         {
             var tenantId = "296b73d9-692a-42bf-9ccb-ff41ca256722";
 
-            _autoMocker.GetMock<IDynamoDBContext>().Setup(x => x.LoadAsync<SettingDbRecord>(tenantId, default)).ReturnsAsync(TestHelper.GetDynamoDBRecord(tenantId));
+            _autoMocker.GetMock<IDynamoDBContext>().Setup(x => x.LoadAsync<SettingDbRecord>(tenantId, default)).ReturnsAsync(_testHelper.GetDynamoDBRecord(tenantId));
 
             var result = await _mockSettingsRepository.GetByIdAsync(Guid.Parse(tenantId));
 
@@ -51,7 +53,7 @@ namespace ServiceName.Test
         {
             var tenantId = "e2c92679-5309-438b-8efc-64054a7babc2";
 
-            _autoMocker.GetMock<IDynamoDBContext>().Setup(x => x.LoadAsync<SettingDbRecord>(tenantId, default)).ReturnsAsync(TestHelper.GetDynamoDBRecord(tenantId));
+            _autoMocker.GetMock<IDynamoDBContext>().Setup(x => x.LoadAsync<SettingDbRecord>(tenantId, default)).ReturnsAsync(_testHelper.GetDynamoDBRecord(tenantId));
 
             var result = await _mockSettingsRepository.GetByIdAsync(Guid.Parse(tenantId));
 
