938051e69dcd49e3e6ab34e044ce0b57ee298700
leandromonaco <leandro.monaco@gmail.com>
2022-06-24 15:03:43 +1000

    [MicroserviceTemplate] Fixed KMS local url Improved Test project Moved tye.yaml to root folder

--- MicroserviceTemplate/tests/ServiceName.Test/Helpers/TestHelper.cs
+++ MicroserviceTemplate/tests/ServiceName.Test/Helpers/TestHelper.cs
@@ -1,52 +1,55 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Amazon;
+using Amazon.KeyManagementService;
+using Microsoft.Extensions.Configuration;
 using ServiceName.Infrastructure.Repositories.DynamoDBModel;
 
 namespace ServiceName.Test.Helpers
 {
     internal class TestHelper
     {
-        public static SettingDbRecord GetDynamoDBRecord(string tenantId)
+        readonly private IConfiguration _configuration;
+
+        public TestHelper()
         {
-            switch (tenantId)
-            {
-                case "53a13ec4-fde8-4087-8e2a-5fb6b1fbc062":
-                    return new SettingDbRecord()
-                    {
-                        TenantId = "53a13ec4-fde8-4087-8e2a-5fb6b1fbc062",
-                        Settings = @"{""CategoryA"":{""IsSettingAEnabled"":true,""IsSettingBEnabled"":true}}"
-                    };
-
-                case "e2c92679-5309-438b-8efc-64054a7babc2":
-                    return new SettingDbRecord()
-                    {
-                        TenantId = "e2c92679-5309-438b-8efc-64054a7babc2",
-                        Settings = String.Empty
-                    };
-
-                default:
-                    return null;
-            }
+            _configuration = new ConfigurationBuilder()
+                                .SetBasePath(Environment.CurrentDirectory)
+                                .AddJsonFile("appsettings.test.json", optional: false, reloadOnChange: true)
+                                .Build();
         }
 
-        internal static IConfigurationRoot GetConfigurationMock()
+        public IConfiguration Configuration
+        { get { return _configuration; } }
+
+        public SettingDbRecord GetDynamoDBRecord(string tenantId)
         {
-            var configDictionary = new Dictionary<string, string>
-                                    {
-                                            {"ModuleConfiguration:Infrastructure:Kms:AccessKey", "test"},
-                                            {"ModuleConfiguration:Infrastructure:Kms:SecretKey", "test"},
-                                            {"ModuleConfiguration:Infrastructure:Kms:RegionEndpoint", "eu-west-2"},
-                                            {"ModuleConfiguration:Infrastructure:Kms:LocalTestEndpoint", "http://localhost:52002"},
-                                            {"ModuleConfiguration:Infrastructure:Kms:SigningKeyId", "6732c7ca-6ec9-4b96-9711-fd1c7d637c8e"},
-                                            {"ModuleConfiguration:Infrastructure:Kms:PublicKey", "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA56tfR6w3YJpbH5XZ6Ze2kB2evnUpbyZJiTLPKSc/VeA46m09lVB7bJRp0pKX2LusT2pccrVe5AYtbnikKqhOQWUdjLJnSONPNpd4yjEseqPblsXicA+xdP+Fk2W0yDxOc79LUAywgjV8JqNbbtVbhzqVPOLalJYnPEAVa3NQV138dnU7NzxbAjPjXINi7BBZ2OLRuocJRMfe16AUiQtH8MaWfRnnRRwdCBLJCXnZy+0hVc701SrVoTS+CA8RfGTCnzutx9MXW7t4SCEjZH0MSfhSZbKggPfi36HeUdClacgD6L0+FhSBKzd8kOC06CDf5WM9oV/XtWVXEWWGPDHv8wIDAQAB"},
-                                            {"ModuleConfiguration:Jwt:Issuer", "Issuer"},
-                                            {"ModuleConfiguration:Jwt:Audience", "Audience"},
-                                    };
+            var dynamoDbData = _configuration.GetSection("TestConfiguration:DynamoDBData").Get<List<SettingDbRecord>>();
+            return dynamoDbData.Where(d => d.TenantId.Equals(tenantId)).FirstOrDefault();
+        }
 
-            var configuration = new ConfigurationBuilder()
-                        .AddInMemoryCollection(configDictionary)
-                        .Build();
+        public IAmazonKeyManagementService GetAmazonKms()
+        {
+            var accessKey = _configuration["ModuleConfiguration:Infrastructure:Kms:AccessKey"];
+            var secretKey = _configuration["ModuleConfiguration:Infrastructure:Kms:SecretKey"];
+            var regionEndpoint = RegionEndpoint.GetBySystemName(_configuration["ModuleConfiguration:Infrastructure:Kms:RegionEndpoint"]);
+            var localTestEndpoint = _configuration["ModuleConfiguration:Infrastructure:Kms:LocalTestEndpoint"];
 
-            return configuration;
+            AmazonKeyManagementServiceConfig amazonKeyManagementServiceConfig = new()
+            {
+                RegionEndpoint = regionEndpoint,
+            };
+
+            if (!string.IsNullOrEmpty(localTestEndpoint))
+            {
+                amazonKeyManagementServiceConfig.UseHttp = true;
+                amazonKeyManagementServiceConfig.ServiceURL = localTestEndpoint;
+            }
+
+            if (!string.IsNullOrEmpty(accessKey) && !string.IsNullOrEmpty(secretKey))
+            {
+                return new AmazonKeyManagementServiceClient(accessKey, secretKey, amazonKeyManagementServiceConfig);
+            }
+
+            return new AmazonKeyManagementServiceClient(amazonKeyManagementServiceConfig);
         }
     }
 }
